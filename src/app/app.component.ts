3a1109811d9690bc79884d75ac25ccbe5d39bd51
jacube1 <jakubfedorczyk@gmail.com>
2019-12-01 22:35:32 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Angular 5';
+  komunikat = 'Ala ma kota';
 }
