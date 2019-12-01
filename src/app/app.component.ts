ea60cf557049ec4653ec9ab00f1d464e5ee35961
jacube1 <jakubfedorczyk@gmail.com>
2019-12-01 23:47:28 +0100

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
 import { Komunikat } from './komunikat';
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent implements OnInit {
+@Component
+(
+  {
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
+  }
+)
+export class AppComponent implements OnInit 
+{
   name = 'Angular 5';
 
   komunikaty: string[];
   pokazKomunikaty = true;
-  nowyKomunikat: Komunikat;
-  ngOnInit() {
+  
+  ngOnInit() 
+  {
     this.komunikaty = [
-  'Ala ma kota',
-  'Kot ma dosc Ali',
-  'Kot i Ala drą ze sobą koty'
+new Komunikat('byle co',new Date(),false),
+new Komunikat('byle co1',new Date(),true),
+new Komunikat('byle co2',new Date(),false)
 ];
-this.nowyKomunikat = new Komunikat('Nowy komunikat', new Date(), true);
   }
 }
