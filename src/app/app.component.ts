76b3d8fb064730a4f4bad807b4616f048d443062
jacube1 <jakubfedorczyk@gmail.com>
2019-12-01 22:42:01 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   name = 'Angular 5';
 
-  komunikat = 'Ala ma kota';
+  komunikat: string;
 
   ngOnInit() {
+    this.komunikat = 'Ala ma kota';
   }
 }
