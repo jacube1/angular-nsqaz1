2aaa2826947c3b041fe0d74798b03fb3c702e7c7
jacube1 <jakubfedorczyk@gmail.com>
2019-12-01 22:37:47 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
+@Component
+(
+  {
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
+  }
+)
+export class AppComponent  
+{
   name = 'Angular 5';
   komunikat = 'Ala ma kota';
 }
